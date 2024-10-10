9a7dcf352750b735b575d5d9a88844665fd43a3a
Christian Vladimir Sucuzhanay Arevalo <sukuzhanay@gmail.com>
2024-10-10 13:12:00 +0200

    added v2 linked js and html

---
+++ saber.js
@@ -0,0 +1,4 @@
+const saber = () => {
+    console.log(document.getElementById('examenes'));
+    document.getElementById('examenes').textContent = 'En papel 20 preguntas';
+    document.getElementsByClassName('botton')[0].textContent = 'Gracias por preguntar';};
